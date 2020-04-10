0a4900a67435478ced9cb7f0d42ec15e0fc59c9a
Anton Chuev <misterchuis@gmail.com>
2020-04-10 16:59:23 +0300

    Add Car class

--- main.dart
+++ main.dart
@@ -1,21 +1,18 @@
 void main(List<String> arguments) {
-  var point = Point(4,6);
-  point.show();
-  hello('Anton');
+  Car car1 = Car("Nissan", model:"350Z", color:"red");
+  Car car2 = Car("BMW", model:"X6");
+  Car car3 = Car("Opel");
 }
 
-class Point{
-  num x, y;
-  Point(this.x, this.y);
-  Point.origin() {
-    x = 0;
-    y = 0;
+class Car {
+  String make;
+  String model;
+  String color;
+  Car(this.make, {this.model, this.color}) {
+    print('${make}${getOptional(model)}${getOptional(color)}'); 
   }
-  show() {
-    print("x = $x\ny = $y");
-  }
-}
 
-hello(name) {
-  print('Hello, $name');
+  String getOptional(String str) {
+    return str == null ? "" : "" + str;
+  }
 }
