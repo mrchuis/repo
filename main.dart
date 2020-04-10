480d915609161c290733063172e972080419c7f5
Anton Chuev <misterchuis@gmail.com>
2020-04-10 09:36:34 +0300

    Changed constructor and added named constructor

--- main.dart
+++ main.dart
@@ -1,13 +1,14 @@
 void main(List<String> arguments) {
-  var point = Point(2,4);
+  var point = Point(5,5);
   point.show();
 }
 
 class Point{
   num x, y;
-  Point(num x, num y) {
-  this.x = x;
-  this.y = y;
+  Point(this.x, this.y);
+  Point.origin() {
+    x = 0;
+    y = 0;
   }
   show() {
     print("x = $x\ny = $y");
