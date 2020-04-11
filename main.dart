053c81a82200eb5c854914a0b8f641bb7493dce7
Anton Chuev <misterchuis@gmail.com>
2020-04-11 13:15:36 +0300

    Exceprion App

--- main.dart
+++ main.dart
@@ -1,18 +1,51 @@
 
 void main(List<String> arguments) {
-  Map<String,String> stringNameByState = {
-    "Msk": "Moscow",
-    "St": "Petersburg",
-    "Ek": "Ekaterinburg",
-    "Now": "Nowgorod"
-  };
+  print('start');
+  List<Cadet> cadetList = [
+    Cadet("Tom", 21),
+    Cadet("Dick", 31),
+    Cadet("Alis", 54),
+    Cadet("Mark", 48)
+  ];
 
-  stringNameByState["Ta"] = "Tagil";
-
-  for (var key in stringNameByState.keys) {
-    print(stringNameByState[key]);    
+  List<Cadet> validCadetList = [];
+  for (Cadet cadet in cadetList) {
+    try {
+      validateCadet(cadet);
+      validCadetList.add(cadet);
+    } on TooOldForServiceExeption catch(ex) {
+      print(ex);
+    }
   }
 
-  print("\nGet just one:${stringNameByState["St"]}");
+  print('finish: ${validCadetList.length} of ${cadetList.length} cadets are valid.');
 }
+
+void validateCadet(Cadet cadet) {
+  if (cadet.age > 45) {
+    throw new TooOldForServiceExeption(cadet);
+  }
+}
+
+class Cadet {
+  String _name;
+  int _age;
+
+  Cadet(this._name, this._age);
+
+  get age => _age;
+  get name => _name;
+}
+
+class TooOldForServiceExeption implements Exception {
+  Cadet _cadet;
+
+  TooOldForServiceExeption(this._cadet);
+
+  toString() {
+    return "${_cadet.name} is too old to be in military service.";
+  }
+}
+
+
 
