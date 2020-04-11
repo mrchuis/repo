f0d7126544ad479f5ed8786de32590a0f178b857
Anton Chuev <misterchuis@gmail.com>
2020-04-11 08:23:29 +0300

    Added Person class

--- main.dart
+++ main.dart
@@ -1,18 +1,26 @@
-void main(List<String> arguments) {
-  Car car1 = Car("Nissan", model:"350Z", color:"red");
-  Car car2 = Car("BMW", model:"X6");
-  Car car3 = Car("Opel");
+
+class Person {
+  String _firstName;
+  String _lastName;
+  String _phone;
+
+  Person(this._firstName, this._lastName, this._phone);
+  toString() {
+    return "$_firstName $_lastName $_phone";
+  }
 }
 
-class Car {
-  String make;
-  String model;
-  String color;
-  Car(this.make, {this.model, this.color}) {
-    print('${make}${getOptional(model)}${getOptional(color)}'); 
-  }
 
-  String getOptional(String str) {
-    return str == null ? "" : "" + str;
-  }
+void main(List<String> arguments) {
+  List<Person> list = [
+    Person("Mark", "Clow", "911"),
+    Person("Alex", "Chuev", "9821"),
+    Person("Anton", "Chuev", "8921")
+  ];
+  print("Not sorted: ${list}");
+
+  list.sort((a,b) => a._firstName.compareTo(b._firstName));
+  print("Sorted by firstname: ${list}");
+  list.sort((a,b) => a._lastName.compareTo(b._lastName));
+  print("Sorted by lastname: ${list}");
 }
